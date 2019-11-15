9ed7e7f8d1ff1a0e68c6037b53a4372d39bb96d5
Brock T <brockthigpen@yahoo.com>
2019-11-15 13:36:18 -0500

    added logic to update rows bg based on client time, hopefully last commit

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,11 +1,20 @@
-let arr1 = ['9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm']
+let arr1 = ['9', '10', '11', '12', '13', '14', '15', '16', '17']
+let dt = new Date();
+let time = dt.getHours();
 // genterate current day
 $('#currentDay').text('Today is ' + moment().format("dddd" + ', ' + "MMM Do"));
 // generate new rows 
 arr1.forEach(function(gen){
     let newRow = $('<div>').addClass('row');
     let newHour = $('<div>').addClass('hour text-center pt-4').text(gen);
-    let newTextarea = $('<textarea>').addClass('past').attr('i', gen);
+    if(parseInt(gen) < 12){
+        $(newHour.text(gen + 'am'))
+    }else if(parseInt(gen) > 12){
+        $(newHour.text(gen - 12 + 'pm'))
+    }else{
+        $(newHour.text(gen + 'pm'))
+    }
+    let newTextarea = $('<textarea>').attr('i', gen);
     let newSaveBtn = $('<button>').addClass('saveBtn fas fa-save fa-lg')
     newRow.append(newHour, newTextarea, newSaveBtn);
     $('.container').append(newRow);
@@ -14,14 +23,24 @@ arr1.forEach(function(gen){
 $(document).ready ( () => {
     $('*[i]').each(function() {
         $(this).val(localStorage.getItem('row - ' + $(this).attr('i')));
+        bgTime();
     });
     // save btn on click
     $('.saveBtn').on('click', function(){
         localStorage.setItem('row - ' + $(this).prev('textarea').attr('i'), $(this).prev('textarea').val());
-        console.log($(this).attr('i'));
-    })
+        
+    });
+    //time based bg color
+    setInterval(bgTime, 60000);
+    function bgTime(){
+        $('*[i]').each(function() {
+            if(parseInt($(this).attr('i')) == time){
+                $(this).removeClass('future past').addClass('present');
+            }else if(parseInt($(this).attr('i')) < time){
+                $(this).removeClass('present future').addClass('past');
+            }else{
+                $(this).removeClass('past present').addClass('future');
+            }
+        });
+    }
 })
-// add click event for local storage
-// check local storage and display any data
-// function that checks client time on a one min interval and color code text areas accordingly....call function in arr1.foreach....put arr1.foreach on a one min interval?....remove past class from textarea in arr1.foreach
-
