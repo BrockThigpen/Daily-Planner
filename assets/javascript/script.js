8f6c96d2c131a621181f471e9d7b7291fd20c493
Brock T <brockthigpen@yahoo.com>
2019-11-15 10:40:37 -0500

    added logic for save btn on click storing to localstorage

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -5,13 +5,21 @@ $('#currentDay').text('Today is ' + moment().format("dddd" + ', ' + "MMM Do"));
 arr1.forEach(function(gen){
     let newRow = $('<div>').addClass('row');
     let newHour = $('<div>').addClass('hour text-center pt-4').text(gen);
-    let newTextarea = $('<textarea>');
+    let newTextarea = $('<textarea>').addClass('past').attr('i', gen);
     let newSaveBtn = $('<button>').addClass('saveBtn fas fa-save fa-lg')
     newRow.append(newHour, newTextarea, newSaveBtn);
     $('.container').append(newRow);
+    
 })
-
+// localStorage
+$(document).ready ( () => {
+    // save btn on click
+    $('.saveBtn').on('click', function(){
+        localStorage.setItem('row - ' + $(this).prev('textarea').attr('i'), $(this).prev('textarea').val());
+        console.log($(this).attr('i'));
+    })
+})
 // add click event for local storage
 // check local storage and display any data
-// add check client time and color code text areas accordingly
+// function that checks client time on a one min interval and color code text areas accordingly....call function in arr1.foreach....put arr1.foreach on a one min interval?....remove past class from textarea in arr1.foreach
 
