800f3bb8c939c7541ddf1da00033df31de3f834f
Brock T <brockthigpen@yahoo.com>
2019-11-15 08:49:38 -0500

    adjusted hour display, added fa icons to save btns

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -4,9 +4,14 @@ $('#currentDay').text('Today is ' + moment().format("dddd" + ', ' + "MMM Do"));
 // generate new rows 
 arr1.forEach(function(gen){
     let newRow = $('<div>').addClass('row');
-    let newHour = $('<div>').addClass('hour').text(gen);
+    let newHour = $('<div>').addClass('hour text-center pt-4').text(gen);
     let newTextarea = $('<textarea>');
-    let newSaveBtn = $('<button>').addClass('saveBtn')
+    let newSaveBtn = $('<button>').addClass('saveBtn fas fa-save fa-lg')
     newRow.append(newHour, newTextarea, newSaveBtn);
     $('.container').append(newRow);
 })
+
+// add click event for local storage
+// check local storage and display any data
+// add check client time and color code text areas accordingly
+
